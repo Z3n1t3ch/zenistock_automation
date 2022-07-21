0cec50c6949ee6348c2b2b11651b68dcbfe67f48
nicolae.scorus <nicolae.scorus@zenitech.co.uk>
2022-07-21 15:22:23 +0300

    ZS2022-18

--- src/main/java/pageObjects/LoginPage.java
+++ src/main/java/pageObjects/LoginPage.java
@@ -2,7 +2,10 @@ package pageObjects;
 
 import driver.DriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
 
 public class LoginPage extends DriverFactory {
 
@@ -11,9 +14,39 @@ public class LoginPage extends DriverFactory {
 
         PageFactory.initElements(driver, this);
     }
+    @FindBy(id = "username")
+    private static WebElement usernameField;
 
-    public void signIn() {
+    @FindBy(id = "password")
+    private static WebElement passwordField;
 
-        driver.get("https://google.com");
+    @FindBy (id = "log-in-button")
+    private static WebElement signInButton;
+
+
+
+
+    public static void successfulSignIn() throws InterruptedException {
+        driver.get("https://zenistock.zenitech.local/login");
+        usernameField. sendKeys("super_admin");
+        passwordField.sendKeys("qwer3#qazxsw09*");
+        signInButton.click();
+        Thread.sleep(3000);
     }
+    public static void invalidLogInNoUsername(){
+        driver.get("https://zenistock.zenitech.local/login");
+        usernameField.sendKeys("");
+        passwordField.sendKeys("qwer3#qazxsw09*");
+        signInButton.click();
+
+
+    }
+    public static void invalidLogInNoPassword(){
+        driver.get("https://zenistock.zenitech.local/login");
+        usernameField.sendKeys("super_admin");
+        passwordField.sendKeys("");
+        signInButton.click();
+    }
+
+
 }
--- src/test/java/pageObjects/LoginPageTests.java
+++ src/test/java/pageObjects/LoginPageTests.java
@@ -4,13 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginPageTests extends BaseTest {
-
     @Test
-    public void signIn() {
-        loginPage.signIn();
+    public void successfulSignIn() throws InterruptedException {
+        LoginPage.successfulSignIn();
         String URL = driver.getCurrentUrl();
-
-        Assert.assertEquals(URL, "https://www.google.com/" );
-        System.out.println("PASSED");
+        Assert.assertEquals(URL, "https://zenistock.zenitech.local/dashboard" );
     }
+    @Test
+    public void logInInvalidUsername(){
+        LoginPage.invalidLogInNoUsername();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL,"https://zenistock.zenitech.local/login");
+
+    }
+    @Test
+    public void logInInvalidPassword(){
+        LoginPage.invalidLogInNoPassword();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, "https://zenistock.zenitech.local/login");
+    }
+
 }
