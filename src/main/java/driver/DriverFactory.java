fa6371688559b32ef7b01ae3dfdc506d061cb572
nicolae.scorus <nicolae.scorus@zenitech.co.uk>
2022-07-22 16:18:08 +0300

    Tests for Login

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -3,15 +3,22 @@ package driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DriverFactory {
 
     public WebDriver driver;
+    public WebDriverWait wait;
 
     public DriverFactory(WebDriver driver) {
-
         this.driver = driver;
+        wait = new WebDriverWait(driver, 20);
     }
+
+
+
 
     @FindBy(id = "username")
     private WebElement usernameField;
@@ -26,20 +33,24 @@ public class DriverFactory {
         usernameField. sendKeys("super_admin");
         passwordField.sendKeys("qwer3#qazxsw09*");
         signInButton.click();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.urlToBe("https://zenistock.zenitech.local/dashboard"));
     }
+
+
+
     public void signInAsAdmin() throws InterruptedException {
         driver.get("https://zenistock.zenitech.local/login");
         usernameField. sendKeys("admin");
         passwordField.sendKeys("qazqwer1@d.");
         signInButton.click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlToBe("https://zenistock.zenitech.local/dashboard"));
     }
     public void signInAsUser() throws InterruptedException {
         driver.get("https://zenistock.zenitech.local/login");
         usernameField. sendKeys("user");
         passwordField.sendKeys("oiuhbvc5%r");
         signInButton.click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlToBe("https://zenistock.zenitech.local/dashboard"));
     }
+
 }
