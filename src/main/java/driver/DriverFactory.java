5759473ba7d5cb4a0a88500d808f784fbef344af
MihaiScorusZeni <109133635+MihaiScorusZeni@users.noreply.github.com>
2022-07-26 09:51:10 +0300

    Zs2022 18 (#10)
    * ZS2022-18
    
    * ZS2022-18
    
    * ZS2022-18V2
    
    * ZS2022-18V3
    
    * login tests v3
    
    * Created wait method and used for tests
    
    * Tests for Login
    
    * Login Tests without target folder
    
    * I corrected all comments, made constants for Credentials and Errors and updated the test cases.
    
    * I corrected all comments, made constants for Credentials and Errors and updated the test cases.
    
    * I corrected all comments, made constants for Credentials and Errors and updated the test cases.
    
    * I corrected all comments, made constants for Credentials and Errors and updated the test cases.v2

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -1,13 +1,49 @@
 package driver;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pageObjects.Credentials;
 
 public class DriverFactory {
 
     public WebDriver driver;
+    public WebDriverWait wait;
 
     public DriverFactory(WebDriver driver) {
-
         this.driver = driver;
+        wait = new WebDriverWait(driver, 20);
     }
+    @FindBy(id = "username")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private  WebElement passwordField;
+
+    @FindBy (id = "log-in-button")
+    private  WebElement signInButton;
+    public void signInAsSuperAdmin() {
+        driver.get(Credentials.login);
+        usernameField.sendKeys(Credentials.usernameSuperAdminRole);
+        passwordField.sendKeys(Credentials.passwordSuperAdminRole);
+        signInButton.click();
+        wait.until(ExpectedConditions.urlToBe(Credentials.dashboard));
+    }
+    public void signInAsAdmin() {
+        driver.get(Credentials.login);
+        usernameField. sendKeys(Credentials.usernameAdminRole);
+        passwordField.sendKeys(Credentials.passwordAdminRole);
+        signInButton.click();
+        wait.until(ExpectedConditions.urlToBe(Credentials.dashboard));
+    }
+    public void signInAsUser() {
+        driver.get(Credentials.login);
+        usernameField. sendKeys(Credentials.usernameUserRole);
+        passwordField.sendKeys(Credentials.passwordUserRole);
+        signInButton.click();
+        wait.until(ExpectedConditions.urlToBe(Credentials.dashboard));
+    }
+
 }
