262f9f8043bd68039e50142805842c15260ce259
darius.chis <darius.chis@zenitech.co.uk>
2022-08-01 14:46:53 +0300

    ZS2022-36 Negative flow test for WarrantyEndDate

--- src/main/java/utils/RandomGenerator.java
+++ src/main/java/utils/RandomGenerator.java
@@ -1,5 +1,10 @@
 package utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
 
@@ -15,4 +20,9 @@ public class RandomGenerator {
         return name.replaceAll("-", "");
     }
 
+    public static String randomDateBefore() throws ParseException {
+        String randomDate = String.valueOf(Instant.now().minus(Duration.ofDays(random.nextInt(365) + 1)));
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(randomDate);
+        return new SimpleDateFormat("MM/dd/yyyy").format(date);
+    }
 }
