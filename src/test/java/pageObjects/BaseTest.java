b4bdb6cc1f32e651d4a2bd666513b8b1f004c719
bianca.joca <bianca.joca@zenitech.co.uk>
2022-08-08 12:06:02 +0300

    Remove an unused import

--- src/test/java/pageObjects/BaseTest.java
+++ src/test/java/pageObjects/BaseTest.java
@@ -1,6 +1,5 @@
 package pageObjects;
 
-import driver.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
